07f7f89f3c671d7893824276a1f8cec40f1d794f
gatchan0807 <yuki.dolphin.koga@gmail.com>
2019-04-24 11:00:58 +0900

    ファイル名の修正

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 
-const Converter = require('./spec/converter');
-// const AccessTester = require('./spec/accessTester');
-const AccessTester = require('./spec/pupetterTester');
+const Converter = require('./src/converter');
+const AccessTester = require('./src/pupetterTester');
 
 fs.readFile('./master/urlLinks.csv', 'utf8', function(err, text) {
   let accessTestList = Converter.convertToJson(text);
